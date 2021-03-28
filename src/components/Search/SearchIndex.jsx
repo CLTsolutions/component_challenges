a3fac69c7f23858e4112621b6e54373a4a0b1209
Chelsey <chelsey.tschida@outlook.com>
2021-03-27 21:13:20 -0400

    refactored code in search

--- src/components/Search/SearchIndex.jsx
+++ src/components/Search/SearchIndex.jsx
@@ -5,17 +5,24 @@ export default class SearchIndex extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards'],
-      searchTerm: ''
+      things: [
+        'pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 
+        'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 
+        'laminator', 'laminating sheets', 'protective sheets', 'index cards'
+      ],
+      results: []
     }
     this.searchFunction = this.searchFunction.bind(this);
   }
 
-  searchFunction(e) {
-    this.setState({ searchTerm: e.target.value })
+  searchFunction(value) {
+    let results = this.state.things.filter(thing => thing.includes(value.toLowerCase()))
+    this.setState({ results: results})
     // console.log(this.state.searchTerm);
   }
   
+  displaySearchItem = (newThings, index) => <p key={index}>{newThings}</p>
+
   render () {
     return (
       <div>
@@ -23,12 +30,15 @@ export default class SearchIndex extends Component {
           type='text'
           value={this.state.searchTerm} 
           placeholder='Search Here'
-          onChange={e => this.searchFunction(e)}  
+          onChange={e => this.searchFunction(e.target.value)}  
         />
         <br />
         <br />
         <h3>Results:</h3>
-          {this.state.things.filter(thing => thing.toLowerCase().includes(this.state.searchTerm.toLowerCase())).map((newThings, index) => <p key={index}>{newThings}</p>)}
+          {this.state.results && this.state.results.length > 0
+            ? this.state.results.map(this.displaySearchItem)
+            : this.state.things.map(this.displaySearchItem)
+          }
       </div>
     )
   }
